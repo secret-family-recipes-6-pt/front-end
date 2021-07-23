5e58fff4801f1cb4e208185efe56b466a3acf228
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-23 14:22:12 -0400

    ENHANCEMENT: Organized the files for better EFFICIENCY  - Created views folder for components that take up full pages

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import HomaPage from "./components/HomePage";
-import AddRecipe from "./components/AddRecipe";
-import SearchBar from "./components/SearchBar"
-import { Button, Toolbar, AppBar, Typography, Grid} from "@material-ui/core";
-
-
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import HomePage from "./views/HomePage";
+import SigninPage from "./views/SigninPage";
 function App() {
-  const logout = () => {};
   return (
     <Router>
-      <div className="App">
-        <AppBar position="static">
-          <Toolbar>
-            <Grid container justifyContent="space-between">
-              <Typography variant="h6">Secret Family Recipes</Typography>
-              <SearchBar />
-              <Button onClick={logout} variant="contained">
-                Logout
-              </Button>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-
-        {
-          //Jose needs to build the private route first.
-          /* <PrivateRoute exact path="/private" component={HomaPage} /> */
-        }
-
-        {
-          //This is the route for login uncomment when done.
-          /* <Route exact path="/" component={Login} /> */
-        }
-        {
-          //will need to be a private eventually
-          <Route exact path="/AddRecipe" component={AddRecipe} />
-        }
+      <div>
+        <Link to="/home">Bring to home</Link>
       </div>
+
+      <Switch>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+
+        <Route path="/">
+          <SigninPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/views/HomePage.js
@@ -0,0 +1,43 @@
+import React from "react";
+// import PrivateRoute from "./components/PrivateRoute";
+import AddRecipe from "../components/AddRecipe";
+import SearchBar from "../components/SearchBar";
+import { Button, Toolbar, AppBar, Typography, Grid } from "@material-ui/core";
+import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
+
+export default function HomePage() {
+  let history = useHistory();
+  const logout = () => {
+    history.push("/");
+  };
+
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container justifyContent="space-between">
+            <Typography variant="h6">Secret Family Recipes</Typography>
+            <SearchBar />
+            <Button onClick={logout} variant="contained">
+              Logout
+            </Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+
+      {
+        //Jose needs to build the private route first.
+        /* <PrivateRoute exact path="/private" component={HomaPage} /> */
+      }
+
+      {
+        //This is the route for login uncomment when done.
+        /* <Route exact path="/" component={Login} /> */
+      }
+      {
+        //will need to be a private eventually
+        <Route exact path="/AddRecipe" component={AddRecipe} />
+      }
+    </div>
+  );
+}
