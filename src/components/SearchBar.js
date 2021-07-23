b92827bd97370dae327def91aaaea1de19c24ed5
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-23 19:28:22 -0400

    from this point on, i will be trying to style using grid

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { InputBase, IconButton, Paper, Select, MenuItem} from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles'
+import React from "react";
+import { InputBase, Paper, Select, MenuItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        padding: '.5rem',
-        width: '60%',
-    }
-
-    
-})
+  root: {
+    display: "flex",
+    padding: ".5rem",
+    width: "60%",
+  },
+});
 
 export default function SearchBar() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Paper component="form" className={classes.root} variant="outlined">
-            <Select autoWidth="true" defaultValue="All Meals" >
-                <MenuItem value="breakfast">Breakfast</MenuItem>
-                <MenuItem value="lunch">Lunch</MenuItem>
-                <MenuItem value="dinner">Dinner</MenuItem>
-                <MenuItem value="dessert">Dessert</MenuItem>
-                <MenuItem value="snacks">Snacks</MenuItem>
-            </Select>
-            <InputBase placeholder="Search" />
-        </Paper>
-    )
+  return (
+    <Paper component="form" className={classes.root} variant="outlined">
+      <Select autoWidth="true" defaultValue="All Meals">
+        <MenuItem value="breakfast">Breakfast</MenuItem>
+        <MenuItem value="lunch">Lunch</MenuItem>
+        <MenuItem value="dinner">Dinner</MenuItem>
+        <MenuItem value="dessert">Dessert</MenuItem>
+        <MenuItem value="snacks">Snacks</MenuItem>
+      </Select>
+      <InputBase placeholder="Search" />
+    </Paper>
+  );
 }
--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,12 +1,105 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { Grid, Paper, Button, TextField, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+// const useStyles = makeStyles({
+//   FormStyle: {
+//     display: "flex",
+//     justifyContent: "space-around",
+//     height: "100%",
+//     flexDirection: "row",
+//   },
+
+//   leftSection: {
+//     width: "45%",
+//   },
+
+//   rightSection: {
+//     width: "45%",
+//   },
+
+//   titleStyle: {
+//     fontSize: "4rem",
+//   },
+
+//   PaperStyle: {
+//     padding: "30px",
+//     height: "70vh",
+//     width: "80%",
+//     margin: "5% auto",
+//   },
+// });
+
+const useStyles = makeStyles((theme) => ({
+  FormStyle: {
+    // display: "flex",
+    // justifyContent: "space-around",
+    // height: "100%",
+    // flexDirection: "column",
+    // alignItems: "center",
+  },
+
+  leftSection: {
+    // width: "45%",
+  },
+
+  rightSection: {
+    // width: "45%",
+  },
+
+  titleStyle: {
+    fontSize: "4rem",
+  },
+
+  PaperStyle: {
+    // padding: "30px",
+    // height: "70vh",
+    // width: "80%",
+    // margin: "5% auto",
+  },
+}));
 
 export default function SignIn() {
+  const classes = useStyles();
+  let history = useHistory();
   return (
-    <div>
-      <div>
-        <Link to="/signup">Don't have an account</Link>
-        <Link to="/home">Log in</Link>
-      </div>
-    </div>
+    <Grid>
+      <Paper elevation={10} className={classes.PaperStyle}>
+        <form className={classes.FormStyle}>
+          {/* left section of form */}
+          <div className={classes.leftSection}>
+            <h2 className={classes.titleStyle}>Sign In</h2>
+
+            {/* email */}
+            <TextField label="Email" placeholder="Enter Email" type="email" />
+
+            {/* password */}
+            <TextField label="Password" placeholder="Enter Password" type="password" />
+          </div>
+
+          {/* right section of form */}
+          <div className={classes.leftSection}>
+            {/* submit login information */}
+            <Button
+              className={classes.ButtonStyle}
+              type="submit"
+              color="primary"
+              variant="contained"
+              onClick={() => {
+                history.push("/home");
+              }}
+            >
+              Sign In
+            </Button>
+
+            <Typography>
+              {" "}
+              Don't have an account?
+              <Link to="/signup"> Sign Up</Link>
+            </Typography>
+          </div>
+        </form>
+      </Paper>
+    </Grid>
   );
 }
