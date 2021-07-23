6e897c1d6b1c6a1de92420b4b7fa74a6b49b7038
unknown <jlarson129@outlook.com>
2021-07-23 17:36:09 -0400

    added divider and styling to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { InputBase, IconButton, Paper, Select, MenuItem} from '@material-ui/core';
+import { InputBase, IconButton, Paper, Select, MenuItem, Divider} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles({
     root: {
         display: 'flex',
-        padding: '.5rem',
-        width: '60%',
+        width: '40%',
+    },
+
+    dropdown: {
+        padding: "0 .5rem",
+        border: "none",
+    },
+
+    divider: {
+        margin: "0 .3rem",
+    },
+
+    searchInput: {
+        width: "100%",
     }
-
-
 
 })
 
@@ -17,8 +27,8 @@ export default function SearchBar() {
     const classes = useStyles();
 
     return (
-        <Paper component="form" className={classes.root} variant="outlined">
-            <Select autoWidth="true" defaultValue="all" placeholder="ALL Meals">
+        <Paper component="form" className={classes.root} variant="outlined" >
+            <Select autoWidth="true" defaultValue="all" placeholder="ALL Meals" className={classes.dropdown} onChange={"change"}>
                 <MenuItem value="all">All Meals</MenuItem>
                 <MenuItem value="breakfast">Breakfast</MenuItem>
                 <MenuItem value="lunch">Lunch</MenuItem>
@@ -26,7 +36,8 @@ export default function SearchBar() {
                 <MenuItem value="dessert">Dessert</MenuItem>
                 <MenuItem value="snacks">Snacks</MenuItem>
             </Select>
-            <InputBase placeholder="Search" />
+            <Divider orientation="vertical" flexItem className={classes.divider}/>
+            <InputBase placeholder="Search" className={`${classes.searchInput}`}/>
         </Paper>
     )
 }
