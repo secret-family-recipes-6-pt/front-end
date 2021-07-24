2745423fd9719674a3e3df5f039ae4dfcb8f3afb
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-23 22:56:59 -0400

    BUGFIX: Register button routed to '/home' instead of '/'

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -56,7 +56,7 @@ export default function SignUp() {
               color="primary"
               variant="contained"
               onClick={() => {
-                history.push("/home");
+                history.push("/");
               }}
               endIcon={<ArrowForwardIcon />}
             >
