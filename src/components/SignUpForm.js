51f2ad6a6c1a844f5e666ed0548e1ce046d875aa
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-25 19:33:27 -0400

    Added form function for SignUp

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,85 @@
+import { Link, useHistory } from "react-router-dom";
+
+import { Grid, Paper, Button, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+
+const useStyles = makeStyles((theme) => ({
+  PapersStyle: {
+    height: "70vh",
+    width: "80%",
+    margin: "5% auto 0 auto",
+  },
+  titleStyle: {
+    fontSize: "5rem",
+    textAlign: "center",
+  },
+  textFieldStyle: {
+    width: "70%",
+    padding: "2px",
+  },
+  ButtonStyle: {
+    marginTop: "5%",
+    height: "20%",
+    width: "60%",
+    borderRadius: "50px",
+    fontSize: "1.75rem",
+  },
+}));
+
+export default function SignUpForm({ values, change, submit, disabled, errors }) {
+  const classes = useStyles();
+  let history = useHistory();
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+    history.push("/");
+  };
+
+  const onChange = (evt) => {
+    // const { name, value, type, checked } = evt.target;
+    // const valueTouse = type === "checkbox" ? checked : value;
+    // change(name, valueTouse);
+
+    const { name, value } = evt.target;
+    change(name, value);
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="signPage-form">
+      <Paper elevation={10} className={classes.PapersStyle}>
+        <Grid container justifyContent="space-around" alignItems="center" className="signPage-grid-container">
+          <Grid className="signPage-grid-item" alignItems="center" item sm={6} xs={12}>
+            <h2 className={classes.titleStyle}>Sign Up</h2>
+
+            {/* username */}
+
+            <TextField id="signup-username" className={classes.textFieldStyle} label="Username" placeholder="Enter Username" type="text" value={values.username} name="username" onChange={onChange} fullWidth />
+
+            {/* email */}
+
+            <TextField id="signup-email" className={classes.textFieldStyle} label="Email" placeholder="Enter Email" type="email" value={values.email} onChange={onChange} name="email" fullWidth />
+
+            {/* password */}
+
+            <TextField id="signup-password" className={classes.textFieldStyle} label="Password" placeholder="Enter Password" type="password" name="password" value={values.password} onChange={onChange} fullWidth />
+          </Grid>
+
+          <Grid className="signPage-grid-item" alignItems="center" item sm={6} xs={12}>
+            <Button className={classes.ButtonStyle} type="submit" color="primary" variant="contained" disabled={disabled} endIcon={<ArrowForwardIcon />}>
+              Register
+            </Button>
+
+            <label style={{ fontSize: "1.5rem" }}>
+              Already have an account?
+              <Link to="/">
+                <span> Sign In</span>
+              </Link>
+            </label>
+          </Grid>
+        </Grid>
+      </Paper>
+    </form>
+  );
+}
--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -1,77 +1,94 @@
-import { Link, useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
 
-import { Grid, Paper, Button, TextField } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+// installed yup and axios; created schema
 
-//  change Paper to Cards component
+import schema from "../validation/signUpSchema";
+import SignUpForm from "../components/SignUpForm";
 
-const useStyles = makeStyles((theme) => ({
-  PapersStyle: {
-    height: "70vh",
-    width: "80%",
-    margin: "5% auto 0 auto",
-  },
-  titleStyle: {
-    fontSize: "5rem",
-    textAlign: "center",
-  },
-  textFieldStyle: {
-    width: "70%",
-    padding: "2px",
-  },
-  ButtonStyle: {
-    marginTop: "5%",
-    height: "20%",
-    width: "60%",
-    borderRadius: "50px",
-    fontSize: "1.75rem",
-  },
-}));
+const initialFormValues = {
+  username: "",
+  email: "",
+  password: "",
+};
+
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+};
+
+const initialDisabled = true;
 
 export default function SignUp() {
-  const classes = useStyles();
-  let history = useHistory();
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+  };
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
+  const postUser = (newUser) => {
+    axios
+      .post("https://secret-family-recipes6.herokuapp.com/api/auth/register", newUser)
+      .then((res) => {
+        console.log(res.data);
+        //set setCurrentUser(res.data)
+      })
+      .catch((err) => {
+        debugger;
+      })
+      .finally(setFormValues(initialFormValues));
+  };
+
+  const formSubmit = () => {
+    /* Axios.get all users. If the newUser has the same username as another, don't allow them to progress to the next page and give matching username error 
+    
+    Break this function and setButton to disabled
+
+    // pass setCurrentUser() to whatever the newuser is, passed down from the parent (App.js). if currentUser = [] then do this
+    */
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+    };
+    postUser(newUser);
+  };
+
   return (
-    <form className="signPage-form">
-      <Paper elevation={10} className={classes.PapersStyle}>
-        <Grid container justifyContent="space-around" alignItems="center" className="signPage-grid-container">
-          <Grid className="signPage-grid-item" alignItems="center" item sm={6} xs={12}>
-            <h2 className={classes.titleStyle}>Sign Up</h2>
-
-            {/* username */}
-            <TextField className={classes.textFieldStyle} label="Username" placeholder="Enter Username" type="input" fullWidth />
-
-            {/* email */}
-            <TextField className={classes.textFieldStyle} label="Email" placeholder="Enter Email" type="email" fullWidth />
-
-            {/* password */}
-            <TextField className={classes.textFieldStyle} label="Password" placeholder="Enter Password" type="password" fullWidth />
-          </Grid>
-
-          <Grid className="signPage-grid-item" alignItems="center" item sm={6} xs={12}>
-            <Button
-              className={classes.ButtonStyle}
-              type="submit"
-              color="primary"
-              variant="contained"
-              onClick={() => {
-                history.push("/");
-              }}
-              endIcon={<ArrowForwardIcon />}
-            >
-              Register
-            </Button>
-
-            <label style={{ fontSize: "1.5rem" }}>
-              Already have an account?
-              <Link to="/">
-                <span> Sign In</span>
-              </Link>
-            </label>
-          </Grid>
-        </Grid>
-      </Paper>
-    </form>
+    <>
+      <SignUpForm values={formValues} change={inputChange} submit={formSubmit} disabled={disabled} errors={formErrors} />
+    </>
   );
 }
