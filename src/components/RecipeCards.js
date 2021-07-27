40c5b7e8b99d5ae752db3d72df95e5ca5dd49ad7
unknown <jlarson129@outlook.com>
2021-07-27 18:54:18 -0400

    fixed some naming issues and finished card filtering function

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -9,15 +9,21 @@ export default function RecipeCards({searchForm}) {
     <Container>
       <Grid spacing={6} container justifyContent="center" alignItems="baseline">
         {cardsInformation.map((cardInfo, index) => {
-          if (category === "all" && searchTerm===""){
+          if (searchCategory === "all" && searchTerm===""){
             return (
               <Grid item key={index} xs={12} md={6} lg={4}>
                 <RecipeCard cardInfo={cardInfo} />
               </Grid>
             );} 
-          else if (cardInfo.name.includes(searchTerm) && (cardInfo.category === searchCategory)){
-
-          }
+          else if (cardInfo.name.includes(searchTerm) && (cardInfo.category === searchCategory || searchCategory === "all")){
+            return (
+              <Grid item key={index} xs={12} md={6} lg={4}>
+                <RecipeCard cardInfo={cardInfo} />
+              </Grid>
+            );}
+            else{
+              //pass
+            }
 
           }
         )}
