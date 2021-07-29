3340e12560296ef93d631b0ac618a431f12acd21
joseguevara29 <guevarabus29@gmail.com>
2021-07-28 22:45:26 -0400

    worked on connecting the backend still a few bugs to fix, added a edit page and some functionality

--- src/views/AddRecipe.js
+++ src/views/AddRecipe.js
@@ -1,7 +1,17 @@
 import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { MenuItem, Select, TextField, Button, Grid } from "@material-ui/core";
+import {
+  MenuItem,
+  Select,
+  TextField,
+  Button,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import { RecipeContext } from "../context/RecipeContext";
+
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,17 +21,70 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-export default function AddEditRecipe() {
-  const { recipe, setRecipe } = useContext(RecipeContext);
 
+export default function AddRecipe() {
+  const { recipe, setRecipe } = useContext(RecipeContext);
+  const history = useHistory();
   const classes = useStyles();
 
-  const [value, setValue] = useState("");
-  const [category, setCategory] = useState("");
+  const handleChange = (e) => {
+    if (e.target.name === "Name") {
+      setRecipe({
+        ...recipe,
+        name: e.target.value,
+      });
+    } else if (e.target.name === "Source") {
+      setRecipe({
+        ...recipe,
+        source: e.target.value,
+      });
+    } else if (e.target.name === "Category") {
+      setRecipe({
+        ...recipe,
+        category: e.target.value,
+      });
+    } else if (e.target.name === "Description") {
+      setRecipe({
+        ...recipe,
+        description: e.target.value,
+      });
+    } else if (e.target.name === "Ingredients") {
+      setRecipe({
+        ...recipe,
+        ingridients: e.target.value,
+      });
+    } else if (e.target.name === "Instructions") {
+      setRecipe({
+        ...recipe,
+        instructions: e.target.value,
+      });
+    }
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-    setCategory(event.target.value);
+    console.log("new recipe: ", recipe);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // saveEdit(editColor);
+
+    axiosWithAuth
+      .post(
+        "https://secret-family-recipes6.herokuapp.com/api/recipes/user/:id",
+        recipe
+      )
+      .then((res) => {
+        console.log("happy path: ", res.data);
+        // localStorage.setItem("token", res.data.token);
+        history.push("/home");
+      })
+      .catch((err) => {
+        console.log("sad path: ", err);
+      });
+  };
+
+  const handleCancel = (e) => {
+    console.log("Cancel button pushed, routing back to home.");
+    history.push("/home");
   };
 
   return (
@@ -34,13 +97,18 @@ export default function AddEditRecipe() {
         justify="center"
         style={{ minHeight: "100vh" }}
       >
+        <Typography variant="h5" component="h2">
+          New Recipe
+        </Typography>{" "}
+        <br />
         <Grid item xs={12}>
           <div>
             <TextField
               id="outlined-basic"
+              name="Name"
               label="Name"
               variant="outlined"
-              // value={value}
+              value={recipe.name}
               onChange={handleChange}
             />
           </div>
@@ -48,9 +116,10 @@ export default function AddEditRecipe() {
             {" "}
             <TextField
               id="outlined-basic"
+              name="Source"
               label="Source"
               variant="outlined"
-              // value={value}
+              value={recipe.source}
               onChange={handleChange}
             />
           </div>
@@ -58,7 +127,8 @@ export default function AddEditRecipe() {
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              value={category}
+              name="Category"
+              value={recipe.category}
               onChange={handleChange}
             >
               <MenuItem value="Select One">Select One</MenuItem>
@@ -72,47 +142,44 @@ export default function AddEditRecipe() {
           <div>
             <TextField
               id="outlined-multiline-static"
+              name="Description"
               label="Description"
               multiline
               rows={8}
               variant="outlined"
-              // value={value}
+              value={recipe.description}
               onChange={handleChange}
             />
           </div>
           <div>
             <TextField
               id="outlined-multiline-static"
+              name="Ingredients"
               label="Ingredients"
               multiline
               rows={8}
               variant="outlined"
-              // value={value}
+              value={recipe.ingredients}
               onChange={handleChange}
             />
           </div>
           <div>
             <TextField
               id="outlined-multiline-static"
+              name="Instructions"
               label="Instructions"
               multiline
               rows={8}
               variant="outlined"
-              // value={value}
+              value={recipe.instructions}
               onChange={handleChange}
             />
           </div>
           <div>
-            <Button
-              // onClick={handleSubmit}
-              variant="contained"
-            >
+            <Button onClick={handleSubmit} variant="contained">
               Save
             </Button>
-            <Button
-              // onClick={cancel}
-              variant="contained"
-            >
+            <Button onClick={handleCancel} variant="contained">
               Cancel
             </Button>
           </div>
