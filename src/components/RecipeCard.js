caa5b3e2ed17651f5c605498f64d5e59b1542c35
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-30 23:01:29 -0400

    Feature: Added Dropdown To Recipe Cards
    The link below shows the changes I made
    https://www.loom.com/share/e91c5ca68d74463ea6a00cdcc30ff410
    
    Used the Complex Interaction example from the link below
    https://material-ui.com/components/cards/
    
    Cards now show Title, Recipe Source, Category, and Description. If the
    user wants to see the Ingrediants & Instructions, they need to click
    on the dropdown.
    
    I also adjusted dummyData.js so you can see title and recipe source when
    rendering components using current RecipeCards.js & RecipeCard.js

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,18 +1,11 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import {
-  Typography,
-  Button,
-  Card,
-  makeStyles,
-  CardContent,
-  CardActions,
-  Grid,
-} from "@material-ui/core";
-import { axiosWithAuth } from "../helpers/axiosWithAuth";
-import axios from "axios";
+import { Typography, Button, Card, makeStyles, CardContent, CardActions, Collapse, Grid, IconButton } from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import clsx from "clsx";
+import { red } from "@material-ui/core/colors";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 100,
   },
@@ -22,62 +15,95 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-});
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 export default function RecipeCard({ cardInfo }) {
   const classes = useStyles();
   const { id } = useParams();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   console.log("id in recipecard component: ", id);
   // console.log("card info id: ", cardInfo.id);
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Secret Recipe
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Title: {cardInfo.recipe_name}
-        </Typography>
-        <Typography className={classes.pos} color="textPrimay">
-          Recipe Source: {cardInfo.recipe_source}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Category: {cardInfo.category}
-        </Typography>
-        <Typography className={classes.pos} variant="body2" component="p">
-          <Typography color="primary">Description</Typography>
+    <>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Secret Recipe
+          </Typography>
+          <Typography variant="h5" component="h2">
+            Title: {cardInfo.recipe_name}
+          </Typography>
+          <Typography className={classes.pos} color="textPrimay">
+            Recipe Source: {cardInfo.recipe_source}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Category: {cardInfo.category}
+          </Typography>
+          <Typography className={classes.pos} variant="body2" component="p">
+            <Typography color="primary">Description</Typography>
 
-          {cardInfo.description}
-        </Typography>
-        <Typography className={classes.pos} variant="body2" component="p">
-          <Typography color="primary">Ingredients</Typography>
-          {cardInfo.ingredients}
-        </Typography>
-        <Typography variant="body2" component="p">
-          <Typography color="primary">Instructions</Typography>
-          {cardInfo.instructions}
-        </Typography>
-      </CardContent>
+            {cardInfo.description}
+          </Typography>
+        </CardContent>
 
-      <CardActions>
-        <Grid container justify="flex-end">
-          <Grid item>
-            <Link
-              to={`/EditRecipe/${cardInfo.id}`}
-              style={{ textDecoration: "none" }}
-            >
-              <Button color="primary" size="small">
-                Edit
-              </Button>
-            </Link>
+        <CardActions disableSpacing>
+          <Grid container justify="space-between">
+            {/* EDIT BUTTON */}
+            <Grid item>
+              <Link to={`/EditRecipe/${cardInfo.id}`} style={{ textDecoration: "none" }}>
+                <Button color="primary" size="small">
+                  Edit
+                </Button>
+              </Link>
+            </Grid>
+
+            {/* EXPAND BUTTON */}
+            <Grid item justify="flex-end">
+              <IconButton onClick={handleExpandClick}>
+                <ExpandMoreIcon
+                  className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                  })}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                />
+              </IconButton>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardActions>
-    </Card>
+        </CardActions>
+
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography className={classes.pos} variant="body2" component="p">
+              <Typography color="primary">Ingredients</Typography>
+              {cardInfo.ingredients}
+            </Typography>
+            <Typography variant="body2" component="p">
+              <Typography color="primary">Instructions</Typography>
+              {cardInfo.instructions}
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </>
   );
 }
